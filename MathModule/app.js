10e21b5d3f892eab027ae92ec6d12aa448c2604f
ziyal <wintermist3@gmail.com>
2017-05-10 11:13:40 -0700

    added Math Module

---
+++ MathModule/app.js
@@ -0,0 +1,5 @@
+var mathModule = require('./math')();
+console.log(mathModule.add(5, 12));
+console.log(mathModule.multiply(5, 12));
+console.log(mathModule.square(5));
+console.log(mathModule.random(5, 12));
